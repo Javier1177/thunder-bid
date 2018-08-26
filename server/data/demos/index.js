b2274b00149ed3f7b524e53971432f5c170bd100
Javi <javierlopezblasco@gmail.com>
2018-08-26 14:33:06 +0200

    Starting demos

--- server/data/demos/index.js
+++ server/data/demos/index.js
@@ -6,3 +6,9 @@ const { Auction, Product, User, WishedList } = require('../models')
 const { mongoose } = require('../../data')
 
 const { env: { MONGO_URL } } = process
+
+mongoose.connect(MONGO_URL)
+    .then(() => mongoose.connection.db.dropDatabase())
+    .then(() => {
+        
+    })
