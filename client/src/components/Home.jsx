b5675426e2682a46bbc722793a32f14618d846d5
Javi <javierlopezblasco@gmail.com>
2018-09-06 13:19:38 +0200

    Home almost finished, need sockets for the price

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,9 +1,37 @@
 import React, { Component } from 'react'
+import logic from '../logic'
+import Product from './Product'
 
 class Home extends Component {
 
     state = {
+        query: '',
+        category: '',
+        products: []
+    }
 
+    componentDidMount(){
+        logic.listProducts(this.state.query, this.state.category)
+            .then(({data}) => {
+                this.setState({
+                products: data
+            })})
+    }
+
+    findProduct = e => {
+        e.preventDefault()
+        logic.listProducts(this.state.query, this.state.category)
+            .then(({data}) => {
+                this.setState({
+                products: data
+            })})
+    }
+
+    resetSearch = () => {
+        this.setState({
+            query: '',
+            category:''
+        })
     }
 
     handleChange = e => {
@@ -15,16 +43,26 @@ class Home extends Component {
 
     render(){
         return <div> 
-            <form >
-                <input type='text' name='surname' onChange={this.handleChange}/>
-                <input type='radio' value='Movie' name='Movie'/>
+            <form onSubmit={this.findProduct}>
+                <input type='text' value={this.state.query} name='query' onChange={this.handleChange}/>
+                <input type='radio' value='' name='category' onChange={this.handleChange} />
+                <label>All</label>
+                <input type='radio' value='Movie' name='category' onChange={this.handleChange}/>
                 <label>Movie</label>
-                <input type='radio' value='Marvel' name='Marvel'/>
+                <input type='radio' value='Marvel' name='category' onChange={this.handleChange}/>
                 <label>Marvel</label>
-                <input type='radio' value='Games' name='Games'/>
+                <input type='radio' value='Games' name='category' onChange={this.handleChange}/>
                 <label>Games</label>
                 <button type='submit'>Submit</button>
+                <button onClick={this.resetSearch}>resetSearch</button>
             </form>
+            <div>
+            {this.state.products === undefined && <h2>There is no products yet!</h2> }
+            {this.state.products != undefined && this.state.products.map(e => {
+                
+                return <Product product={e}/>
+            })}
+            </div>
         </div>
     }
 }
---
+++ client/src/components/Product.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react"
+
+class Product extends Component {
+    render() {
+        return <div>
+            <h2>{this.props.product.title}</h2>
+            <ul>
+                <li><img src={this.props.product.image} height='200' width='100'></img></li>            
+                <li>{this.props.product.description}</li>
+            </ul>
+          </div>
+  
+        }
+      }
+
+export default Product
