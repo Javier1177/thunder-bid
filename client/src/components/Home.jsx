a04633ad94a727a101b9571e33190f8e742d0621
Javi <javierlopezblasco@gmail.com>
2018-09-06 12:11:38 +0200

    starting home

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,8 +1,31 @@
 import React, { Component } from 'react'
 
 class Home extends Component {
+
+    state = {
+
+    }
+
+    handleChange = e => {
+        const {name, value} = e.target
+        this.setState({
+            [name]: value
+        })
+    }
+
     render(){
-        return <div> Esto es la home</div>
+        return <div> 
+            <form >
+                <input type='text' name='surname' onChange={this.handleChange}/>
+                <input type='radio' value='Movie' name='Movie'/>
+                <label>Movie</label>
+                <input type='radio' value='Marvel' name='Marvel'/>
+                <label>Marvel</label>
+                <input type='radio' value='Games' name='Games'/>
+                <label>Games</label>
+                <button type='submit'>Submit</button>
+            </form>
+        </div>
     }
 }
 
